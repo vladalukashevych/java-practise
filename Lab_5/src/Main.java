66746af3b9c0dfc329cd4f0584147bbb59fd1f4d
Andrii Sakhno <andreylion.zt@gmail.com>
2023-11-26 14:55:44 +0100

    Changed naming

---
+++ Lab_5/src/Main.java
@@ -0,0 +1,14 @@
+import com.education.ztu.Product;
+import com.education.ztu.Task3;
+import com.education.ztu.Task4;
+import com.education.ztu.Task5;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Task3.DoTask();
+        //Task4.DoTask();
+        Task5.DoTask();
+    }
+}
